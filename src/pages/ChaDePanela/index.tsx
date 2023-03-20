e5d2fa1c2ba7fc1f2fc7db7a47b724fd052adc4f
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-20 16:02:02 -0300

    fix: link chabar

--- src/pages/ChaDePanela/index.tsx
+++ src/pages/ChaDePanela/index.tsx
@@ -2,11 +2,10 @@ export function ChaDePanela() {
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <iframe
-        src="https://www.instagram.com/p/CpV35lKD0jy/embed"
+        src="http://beatrizealexander.meuchadepanela.com.br/"
         width="100%"
         height="100%"
         allowFullScreen
-        style={{ borderRadius: '10px' }}
         title="Publicação incorporada"
       ></iframe>
     </div>
