7c29bc5fcdfaa0745af10b1df793368dde954b48
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-20 15:54:17 -0300

    feat: add iframe list chabar

---
+++ src/pages/ChaDePanela/index.tsx
@@ -0,0 +1,14 @@
+export function ChaDePanela() {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <iframe
+        src="http://beatrizealexander.meuchadepanela.com.br/#/"
+        width="100%"
+        height="100%"
+        allowFullScreen
+        style={{ borderRadius: '10px' }}
+        title="Publicação incorporada"
+      ></iframe>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import ViteRadar from 'vite-plugin-radar';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ViteRadar({
+      analytics: {
+        id: 'G-9WPX9XHJDG',
+      },
+    }),
+  ],
 });
