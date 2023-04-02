ef312f84b72dd3c7c1ce1a08cc41e33926f326f5
ialexanderbrito <xanderzinho26@gmail.com>
2023-04-02 10:55:22 -0300

    feat: add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import { ChaDePanela } from 'pages/ChaDePanela';
 import { Homepage } from 'pages/Home';
+import { NotFound } from 'pages/NotFound';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="/chabar" element={<ChaDePanela />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
