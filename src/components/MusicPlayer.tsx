f9b039702825a04caf423df27309eb18b97bac86
ialexanderbrito <xanderzinho26@gmail.com>
2023-05-30 23:20:03 -0300

    fix: button show volume

--- src/components/MusicPlayer.tsx
+++ src/components/MusicPlayer.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 import ReactHowler from 'react-howler';
-import { BiPlay, BiPause, BiPlus, BiMinus } from 'react-icons/bi';
+import {
+  BiPlay,
+  BiPause,
+  BiPlus,
+  BiMinus,
+  BiChevronRight,
+  BiChevronLeft,
+} from 'react-icons/bi';
 
 import music from 'assets/wedding.mp3';
 
@@ -11,6 +18,7 @@ interface MusicPlayerProps {
 
 export function MusicPlayer({ play, setPlay }: MusicPlayerProps) {
   const [volumeChange, setVolumeChange] = useState(0.5);
+  const [showVolume, setShowVolume] = useState(false);
 
   function volumePorcentage() {
     const porcentage = Math.round(volumeChange * 100);
@@ -25,7 +33,7 @@ export function MusicPlayer({ play, setPlay }: MusicPlayerProps) {
     <>
       <ReactHowler src={music} playing={play} loop volume={volumeChange} />
 
-      <div className="fixed bottom-0 right-0 z-50 mb-3 mr-3 flex gap-4 md:mb-8 md:mr-8">
+      <div className="fixed bottom-0 right-0 z-50 mb-3 mr-3 flex gap-2 md:mb-8 md:mr-8">
         <button
           className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
           aria-label="audio-control"
@@ -35,30 +43,48 @@ export function MusicPlayer({ play, setPlay }: MusicPlayerProps) {
         >
           {play ? <BiPause /> : <BiPlay />}
         </button>
-        <button
-          className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
-          aria-label="audio-control"
-          onClick={() => {
-            if (volumeChange <= 0) return;
-            setVolumeChange(volumeChange - 0.1);
-          }}
-        >
-          <BiMinus className="text-2xl" />
-        </button>
-        <button
-          className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
-          aria-label="audio-control"
-          onClick={() => {
-            if (volumeChange >= 1) return;
-            setVolumeChange(volumeChange + 0.1);
-          }}
-        >
-          <BiPlus className="text-2xl" />
-        </button>
+        {showVolume && (
+          <>
+            <button
+              className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
+              aria-label="audio-control"
+              onClick={() => {
+                if (volumeChange <= 0) return;
+                setVolumeChange(volumeChange - 0.1);
+              }}
+            >
+              <BiMinus className="text-2xl" />
+            </button>
+            <button
+              className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
+              aria-label="audio-control"
+              onClick={() => {
+                if (volumeChange >= 1) return;
+                setVolumeChange(volumeChange + 0.1);
+              }}
+            >
+              <BiPlus className="text-2xl" />
+            </button>
 
-        <div className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-lg drop-shadow-md md:h-11 md:w-11">
-          {volumePorcentage()}
-        </div>
+            <div className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-lg drop-shadow-md md:h-11 md:w-11">
+              {volumePorcentage()}
+            </div>
+          </>
+        )}
+
+        <button
+          className="flex h-9 w-9 items-center justify-center rounded-full bg-septenary text-4xl drop-shadow-md md:h-11 md:w-11"
+          aria-label="audio-control"
+          onClick={() => {
+            setShowVolume(!showVolume);
+          }}
+        >
+          {showVolume ? (
+            <BiChevronRight className="text-2xl" />
+          ) : (
+            <BiChevronLeft className="text-2xl" />
+          )}
+        </button>
       </div>
     </>
   );
