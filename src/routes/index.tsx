d6af4460271048fc1ea1adf8e910557c7ea1ffc7
ialexanderbrito <xanderzinho26@gmail.com>
2023-07-17 18:22:41 -0300

    feat: add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { CasasBahia } from 'pages/CasasBahia';
-import { ChaDePanela } from 'pages/ChaDePanela';
+import { Filtro } from 'pages/Filtro';
 import { Homepage } from 'pages/Home';
 import { Links } from 'pages/Links';
 import { NotFound } from 'pages/NotFound';
+import { NuBank } from 'pages/NuBank';
 import { Riachuelo } from 'pages/Riachuelo';
+import { Rotas } from 'pages/Routes';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/chabar" element={<ChaDePanela />} />
       <Route path="/riachuelo" element={<Riachuelo />} />
       <Route path="/casasbahia" element={<CasasBahia />} />
+      <Route path="/pix" element={<NuBank />} />
+      <Route path="/filtro" element={<Filtro />} />
       <Route path="/links" element={<Links />} />
+      <Route path="/rotas" element={<Rotas />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
